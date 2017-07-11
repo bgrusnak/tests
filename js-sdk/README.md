247ddf729a529963d3a4cf475f6c8481cfd45e7b
Ilya A. Shlyakhovoy <bulgarus@inbox.ru>
2017-07-11 20:13:38 +0300

    Update

--- js-sdk/README.md
+++ js-sdk/README.md
@@ -1,2 +1,45 @@
-# js-sdk test
+## React test js-sdk
 
+### Install all dependencies
+-----
+
+```
+yarn
+```
+or
+```
+npm install
+```
+
+### Start project on local server (go to localhost:1337)
+-----
+
+```
+yarn start
+```
+or
+```
+npm start
+```
+
+### Build project (in "dist" folder):
+-----
+
+```
+yarn run build
+```
+or
+```
+npm run build
+```
+
+### Preview minified production-ready bundle (go to localhost:3000)
+-----
+
+```
+yarn run preview
+```
+or
+```
+npm run preview
+```
--- js-sdk/webpack.config.js
+++ js-sdk/webpack.config.js
@@ -1,43 +1,103 @@
-var webpack = require('webpack');
-var path = require('path');
-var loaders = require('./webpack.loaders');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: [
-		'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
-		'webpack/hot/only-dev-server',
-		'./src/index.jsx' // Your appʼs entry point
-	],
-	devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	resolve: {
-		extensions: ['', '.js', '.jsx']
-	},
-	module: {
-		loaders: loaders.concat([{
-			test: /\.css$/,
-			loaders: [
-				'style?sourceMap',
-				'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
-			]
-		}])
-	},
-	devServer: {
-		contentBase: "./public",
-		noInfo: true, //  --no-info option
-		hot: true,
-		inline: true
-	},
-	plugins: [
-		new webpack.NoErrorsPlugin(),
-		new CopyWebpackPlugin([{
-			from: './src/index.html'
-		}])
-	]
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://0.0.0.0:1337',
+    'webpack/hot/only-dev-server',
+    'babel-polyfill',
+    'whatwg-fetch',
+    './src/index.js'
+  ],
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: process.env.PORT || 1337,
+    host: '0.0.0.0',
+    publicPath: '/',
+    historyApiFallback: true,
+    disableHostCheck: true
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'app.[hash].js'
+  },
+  devtool: 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              'es2015',
+              {
+                'modules': false
+              }
+            ],
+            'stage-0',
+            'react'
+          ],
+          plugins: [
+            'transform-async-to-generator',
+            'transform-decorators-legacy'
+          ]
+        }
+      },
+      {
+        test: /\.scss|css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'resolve-url-loader',
+          'sass-loader?sourceMap'
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          {
+            loader: 'image-webpack-loader',
+            query: {
+              mozjpeg: {
+                progressive: true
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              optipng: {
+                optimizationLevel: 4
+              },
+              pngquant: {
+                quality: '75-90',
+                speed: 3
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'file-loader',
+        exclude: /images/,
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({ hash: false, template: './index.hbs' }),
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /nb/)
+  ]
 };
